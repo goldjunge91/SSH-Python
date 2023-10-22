12a1c789c0bf56322b8d71ed1d881abe413a7eed
Goldjunge91 <m.tozzi91@icloud.com>
2023-10-22 09:55:33 +0200

    added changed copy to server not working

--- copy_to_server_tab.py
+++ copy_to_server_tab.py
@@ -1,10 +1,38 @@
 import tkinter as tk
 from tkinter import ttk, messagebox, filedialog
 import subprocess
+import platform
 
-def copy_key_to_server(hostname, port, username, ssh_key_path):
-    """Copy the SSH key to the server using ssh-copy-id."""
-    # ... [rest of the function remains unchanged]
+def check_host_connectivity(hostname):
+    """Check if the host is reachable using ping."""
+    command = f'ping -n 1 {hostname}'
+    try:
+        subprocess.run(command, check=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+def copy_key_to_server(hostname, port, username, ssh_key_path, output_label, method="ssh-copy-id"):
+    """Copy the SSH key to the server."""
+    # Check connectivity first
+    if not check_host_connectivity(hostname):
+        output_label.config(text=f"Error: Unable to connect to {hostname}.", foreground="red")
+        return
+
+    # Determine the command based on the selected method
+    if method == "ssh-copy-id":
+        command = f'ssh-copy-id -i {ssh_key_path} {username}@{hostname} -p {port}'
+    else:
+        command = f'cat {ssh_key_path}.pub | ssh {username}@{hostname} -p {port} "mkdir -p ~/.ssh && chmod 700 ~/.ssh && cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"'
+
+    # Determine the platform and run the command in a new terminal window
+    os_type = platform.system()
+    if os_type == "Windows":
+        subprocess.Popen(['cmd.exe', '/k', command])
+    elif os_type == "Linux":
+        subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', command])
+    elif os_type == "Darwin":  # macOS
+        subprocess.Popen(['osascript', '-e', f'tell app "Terminal" to do script "{command}"'])
 
 def create_copy_to_server_tab(notebook, ssh_key_entry):
     """Create and return the modified Copy to Server tab."""
@@ -45,12 +73,17 @@ def create_copy_to_server_tab(notebook, ssh_key_entry):
     browse_btn = ttk.Button(copy_key_frame, text="Browse File", command=browse_file)
     browse_btn.pack(padx=10, pady=5)
 
+    # Output Line
+    output_label = ttk.Label(copy_key_frame, text="")
+    output_label.pack(padx=10, pady=10, fill=tk.X)
+
     # Button to copy key
     copy_btn = ttk.Button(copy_key_frame, text="Copy Key", command=lambda: copy_key_to_server(
         hostname_entry.get(),
         port_entry.get(),
         username_entry.get(),
-        ssh_key_path_entry.get()
+        ssh_key_path_entry.get(),
+        output_label
     ))
     copy_btn.pack(padx=10, pady=20)
 
