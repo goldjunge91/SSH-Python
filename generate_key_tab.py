211b3dc74bc78012517b95f719436e5348a559e5
Goldjunge91 <m.tozzi91@icloud.com>
2023-10-22 07:58:12 +0200

    generate_key_tab update alle schlüssel Typen funktionieren.

--- generate_key_tab.py
+++ generate_key_tab.py
@@ -3,12 +3,29 @@ from tkinter import ttk, filedialog, messagebox
 import subprocess
 import os
 
+def update_bit_lengths(key_type, bit_length_dropdown):
+    """Update the available bit lengths based on the selected key type."""
+    bit_lengths = {
+        "rsa": ["1024", "2048", "3072", "4096"],
+        "dsa": ["1024"],
+        "ecdsa": [],  # ECDSA doesn't use the -b option
+        "ed25519": []  # ED25519 doesn't use the -b option
+    }
+    bit_length_dropdown["values"] = bit_lengths[key_type]
+    if key_type in ["ecdsa", "ed25519"]:
+        bit_length_dropdown.set("")
+    else:
+        bit_length_dropdown.set(bit_lengths[key_type][0])
+
 def generate_ssh_key(options):
     """Generate a new SSH key using ssh-keygen."""
-    path = options["-f"].get()
-    if not path:
-        messagebox.showerror("Error", "Please specify a path for the SSH key.")
+    directory = options["directory"].get()
+    filename = options["-f"].get()
+    if not directory or not filename:
+        messagebox.showerror("Error", "Please specify both the directory and filename for the SSH key.")
         return
+
+    path = os.path.join(directory, filename)
 
     # Ensure we don't overwrite an existing key
     counter = 1
@@ -22,8 +39,8 @@ def generate_ssh_key(options):
 
     # Add options to the command
     for flag, value in options.items():
-        if value.get():
-            if flag == "-b" and options["-t"].get() not in ["rsa", "dsa", "ecdsa"]:
+        if flag != "directory" and value.get():
+            if flag == "-b" and options["-t"].get() not in ["rsa", "dsa"]:
                 continue
             command.extend([flag, value.get()])
 
@@ -33,8 +50,6 @@ def generate_ssh_key(options):
     except Exception as e:
         messagebox.showerror("Error", str(e))
 
-# Rest of the code remains the same...
-
 def create_generate_key_tab(notebook):
     """Create and return the Generate Key tab."""
     generate_key_frame = ttk.Frame(notebook)
@@ -42,7 +57,8 @@ def create_generate_key_tab(notebook):
     options = {
         "-t": tk.StringVar(value="rsa"),  # Key type
         "-b": tk.StringVar(value="2048"),  # Bit length
-        "-f": tk.StringVar(),  # File path
+        "directory": tk.StringVar(),  # Directory
+        "-f": tk.StringVar(),  # Filename
         "-C": tk.StringVar(),  # Comment
         "-N": tk.StringVar()   # Passphrase
     }
@@ -52,18 +68,23 @@ def create_generate_key_tab(notebook):
     key_types = ["rsa", "dsa", "ecdsa", "ed25519"]
     key_type_dropdown = ttk.Combobox(generate_key_frame, textvariable=options["-t"], values=key_types, state="readonly")
     key_type_dropdown.pack(padx=10, pady=5)
+    key_type_dropdown.bind("<<ComboboxSelected>>", lambda e: update_bit_lengths(key_type_dropdown.get(), bit_length_dropdown))
 
     # Bit Length Dropdown
     ttk.Label(generate_key_frame, text="Bit Length:").pack(padx=10, pady=5)
-    bit_lengths = ["1024", "2048", "3072", "4096"]
-    bit_length_dropdown = ttk.Combobox(generate_key_frame, textvariable=options["-b"], values=bit_lengths, state="readonly")
+    bit_length_dropdown = ttk.Combobox(generate_key_frame, textvariable=options["-b"], values=["1024", "2048", "3072", "4096"], state="readonly")
     bit_length_dropdown.pack(padx=10, pady=5)
 
-    # File Path Entry with Browse button
-    ttk.Label(generate_key_frame, text="File Path:").pack(padx=10, pady=5)
-    file_path_entry = ttk.Entry(generate_key_frame, textvariable=options["-f"])
-    file_path_entry.pack(padx=10, pady=5, side=tk.LEFT)
-    ttk.Button(generate_key_frame, text="Browse", command=lambda: select_save_path(options["-f"])).pack(padx=10, pady=5, side=tk.LEFT)
+    # Directory Entry with Browse button
+    ttk.Label(generate_key_frame, text="Directory:").pack(padx=10, pady=5)
+    directory_entry = ttk.Entry(generate_key_frame, textvariable=options["directory"])
+    directory_entry.pack(padx=10, pady=5, side=tk.LEFT)
+    ttk.Button(generate_key_frame, text="Browse", command=lambda: select_directory(options["directory"])).pack(padx=10, pady=5, side=tk.LEFT)
+
+    # Filename Entry
+    ttk.Label(generate_key_frame, text="Filename:").pack(padx=10, pady=5)
+    filename_entry = ttk.Entry(generate_key_frame, textvariable=options["-f"])
+    filename_entry.pack(padx=10, pady=5)
 
     # Comment Entry
     ttk.Label(generate_key_frame, text="Comment:").pack(padx=10, pady=5)
@@ -80,8 +101,8 @@ def create_generate_key_tab(notebook):
 
     return generate_key_frame
 
-def select_save_path(var):
-    """Open a file dialog to select the save path and update the given variable."""
-    filepath = filedialog.asksaveasfilename(defaultextension=".key", filetypes=[("Key Files", "*.key"), ("All Files", "*.*")])
-    if filepath:
-        var.set(filepath)
+def select_directory(var):
+    """Open a directory dialog to select the save directory and update the given variable."""
+    directory = filedialog.askdirectory()
+    if directory:
+        var.set(directory)
